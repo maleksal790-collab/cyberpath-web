082eea1ebb9a97b9197ca0b9ed1c47f3764cb073
Maleksumaili <289015421+maleksal790-collab@users.noreply.github.com>
2026-08-08 08:47:56 +0300

    Initialize PostHog analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import posthog from 'posthog-js'
 import App from './App'
 import './index.css'
+
+posthog.init('phc_ybydTymP5Ui953JHB8jG8u8papkK4SsmSXcRQgr6h7g4', {
+  api_host: 'https://us.i.posthog.com',
+  person_profiles: 'identified_only',
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
